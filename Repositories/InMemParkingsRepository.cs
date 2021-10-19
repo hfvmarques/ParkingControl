83ce02f14b2e6f98103c13499c31954ebbf459c7
Henrique Marques <hfvmarques@gmail.com>
2021-10-18 22:40:50 -0400

    added in memory parkings repository

---
+++ Repositories/InMemParkingsRepository.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using ParkingControl.Entities;
+
+namespace ParkingControl.Repositories
+{
+  public class InMemParkingsRepository
+  {
+    private readonly List<Parking> parkings = new()
+    {
+      new Parking
+      {
+        Id = 1,
+        Plate = "AAA-1234",
+        EntryTime = System.DateTimeOffset.UtcNow
+      },
+      new Parking
+      {
+        Id = 2,
+        Plate = "BBB-1234",
+        EntryTime = System.DateTimeOffset.UtcNow
+      },
+      new Parking
+      {
+        Id = 3,
+        Plate = "CCC-1234",
+        EntryTime = System.DateTimeOffset.UtcNow
+      }
+    };
+
+    public IEnumerable<Parking> GetParkings()
+    {
+      return parkings;
+    }
+
+    public Parking GetParking(int id)
+    {
+      return parkings.Where(parking => parking.Id == id).SingleOrDefault();
+    }
+  }
+}
