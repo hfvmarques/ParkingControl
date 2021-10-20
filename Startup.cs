577d21a4c665891ed8f163e31896e23a4797ac3e
Henrique Marques <hfvmarques@gmail.com>
2021-10-20 17:27:09 -0400

    implemented user and password on mongodb and health checks endpoits

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -34,10 +38,12 @@ namespace ParkingControl
     {
       BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+      var mongoDBSettings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+
       services.AddSingleton<IMongoClient>(serviceProvider =>
       {
-        var settings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-        return new MongoClient(settings.ConnectionString);
+
+        return new MongoClient(mongoDBSettings.ConnectionString);
       });
 
       services.AddSingleton<IParkingsRepository, MongoDBParkingsRepository>();
@@ -50,6 +56,12 @@ namespace ParkingControl
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParkingControl", Version = "v1" });
       });
+
+      services.AddHealthChecks().AddMongoDb(
+        mongoDBSettings.ConnectionString,
+        name: "mongodb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] { "ready" });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +83,32 @@ namespace ParkingControl
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+
+        endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+        {
+          Predicate = (check) => check.Tags.Contains("ready"),
+          ResponseWriter = async (context, report) =>
+          {
+            var result = JsonSerializer.Serialize(new
+            {
+              status = report.Status.ToString(),
+              checks = report.Entries.Select(entry => new
+              {
+                name = entry.Key,
+                status = entry.Value.Status.ToString(),
+                exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                duration = entry.Value.Duration.ToString()
+              })
+            });
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+          }
+        });
+
+        endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+        {
+          Predicate = (_) => false
+        });
       });
     }
   }
