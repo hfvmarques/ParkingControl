836d17678cff4d951d6d5c5971ca84e6d5244523
Henrique Marques <hfvmarques@gmail.com>
2021-10-24 22:28:31 -0400

    applied display format on EntryDate and ExitDate

--- ParkingControl.Api/DTOs.cs
+++ ParkingControl.Api/DTOs.cs
@@ -6,7 +6,11 @@ namespace ParkingControl.Api.DTOs
   public record ParkingDTO(
     int Id,
     string Plate,
+    [RegularExpression(@"([1-9]|([012][0-9])|(3[01]))/([0]{0,1}[1-9]|1[012])/\d\d\d\d 
+    (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d")]
     DateTimeOffset EntryDate,
+    [RegularExpression(@"([1-9]|([012][0-9])|(3[01]))/([0]{0,1}[1-9]|1[012])/\d\d\d\d 
+    (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d")]
     Nullable<DateTimeOffset> ExitDate,
     Boolean Paid,
     Boolean Left);
--- ParkingControl.Api/Entities/Parking.cs
+++ ParkingControl.Api/Entities/Parking.cs
@@ -7,7 +7,11 @@ namespace ParkingControl.Api.Entities
   {
     public int Id { get; set; }
     public string Plate { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
     public DateTimeOffset EntryDate { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
     public Nullable<DateTimeOffset> ExitDate { get; set; }
     public Boolean Paid { get; set; }
     public Boolean Left { get; set; }
